f87d8256f1ddfa1d7c1421be3cfb3f6af8be6c4f
Tim Wilder <tmwilder@gmail.com>
2016-11-15 00:00:01 -0800

    Initial commit of first two steps

--- js/main.js
+++ js/main.js
@@ -1,15 +1,18 @@
 var calculateMonthlyPayment = function (principal, years, rate) {
+    let monthlyRate = 0;
+
     if (rate) {
-        var monthlyRate = rate / 100 / 12;
+        monthlyRate = rate / 100 / 12;
     }
-    var monthlyPayment = principal * monthlyRate / (1 - (Math.pow(1 / (1 + monthlyRate), years * 12)));
-    return monthlyPayment;
+    let monthlyPayment = principal * monthlyRate / (1 - (Math.pow(1 / (1 + monthlyRate), years * 12)));
+    return {principal, years, rate, monthlyPayment, monthlyRate};
 };
 
 document.getElementById('calcBtn').addEventListener('click', function () {
     var principal = document.getElementById("principal").value;
     var years = document.getElementById("years").value;
     var rate = document.getElementById("rate").value;
-    var monthlyPayment = calculateMonthlyPayment(principal, years, rate);
+    let {monthlyPayment, monthlyRate} = calculateMonthlyPayment(principal, years, rate);
     document.getElementById("monthlyPayment").innerHTML = monthlyPayment.toFixed(2);
+    document.getElementById("monthlyRate").innerHTML = (monthlyRate * 100).toFixed(2);
 });
